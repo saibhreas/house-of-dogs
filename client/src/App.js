1638a5be808f779bc6c02a9344cd04447959ce89
siobhan <sr@saibhreas.com>
2021-07-09 15:13:33 -0400

    cleaned up react

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to the House of Dogs</h1>
+      <p> npm start    to run</p>
+    </div>
+  );
+};
+
+export default App;
