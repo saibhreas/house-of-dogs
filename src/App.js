a5db68c25211f56d2db5890df8690942afef8d3f
siobhan <sr@saibhreas.com>
2021-07-09 14:11:46 -0400

    start of front end client side

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my blank React API</h1>
+      <p> npm start    to run</p>
+    </div>
+  );
+};
+
+export default App;
