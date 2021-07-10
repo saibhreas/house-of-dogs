2a2cf7ac14309963b4892ac69e204522da5ce842
siobhan <sr@saibhreas.com>
2021-07-10 16:54:04 -0400

    trying to get seeds to work, helper left.  saving and committing will come back in a bit

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,11 +1,11 @@
 const db = require('../config/connection');
-const { User, ServiceProvider, Pets } = require('../models');
-const ProfileSeeds = require('./Profile.json');
+const { User, Provider, Dog } = require('../models');
+const profileSeeds = require('./Profile.json');
 
 db.once('open', async () => {
   try {
     await User.deleteMany({});
-    await User.create(ProfileSeeds);
+    await User.create(profileSeeds);
 
     console.log('all done!');
     process.exit(0);
