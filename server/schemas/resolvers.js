24730838205aba3de587847dcb9348dd9ca853fb
siobhan <sr@saibhreas.com>
2021-07-11 17:28:29 -0400

    fixing typedefs & resovers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
-const { Profile } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { Dog, Provider, Service, User, Veterinarian} = require('../models');
+const { signToken } = require('../utils/auth');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');const { User, Dog, Provider, Service, Veterinarian} = require('../models');
 
 const resolvers = {
   Query: {
