2782d19bfb54ece001af56cd01346a5a51ab8123
C. Rando <crando@gmail.com>
2021-07-11 09:07:41 -0400

    updated server information

--- server/models/index.js
+++ server/models/index.js
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
-const User = require('./User');
-
-module.exports = { User };
-=======
 const User = require ('./User');
 const Provider = require('./Provider');
 const Dog = require('./Dog');
@@ -12,4 +7,3 @@ const Service = require('./Service');
 module.exports = { User, Provider, Dog, Veterinarian, Service };
 
 
->>>>>>> 2e7aae5d48d174ac432fc20ffe663f879046cde4
