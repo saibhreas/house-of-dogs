f6016e6b5424dafe89e085904a30d4fb5a358a7e
siobhan <sr@saibhreas.com>
2021-07-11 17:25:51 -0400

    fixing modles + typedef

--- server/server.js
+++ server/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+const db = require('./config/connection');
+
 
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
