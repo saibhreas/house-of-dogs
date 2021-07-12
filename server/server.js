d86b45df2e764418feea07bee4d00afa17ef7137
siobhan <sr@saibhreas.com>
2021-07-11 20:00:28 -0400

    Looking ql error

--- server/server.js
+++ server/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-
-
 const { typeDefs, resolvers } = require('./schemas');
+
+const db = require('./config/connection');
 
 
 const PORT = process.env.PORT || 3001;
